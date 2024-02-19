4e3861012dff2b8c32483516598c278064ebc1a2
律澗隱 <jianyin@lujianyindeMacBook-Pro.local>
2024-02-19 16:47:19 +0800

    update login.py interface

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # 應用程序入口
+import tkinter as tk
+from ui.login import login_window # 引入登录窗口
+
+def main():
+    root = tk.Tk()
+    login_window(root)
+    root.mainloop()
+    
+if __name__ == '__main__':
+    main()
---
+++ ui/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+self.login_button = tk.Button(self.root, image=self.login_button_image, borderwidth=0, command=self.login)
+        # self.login_button.pack(pady=10)
